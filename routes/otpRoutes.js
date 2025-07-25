9681832cc7728f6bf04cb83ee0e5c8b43ac945a5
Ramesh Jana <81910860+Ramesh2911@users.noreply.github.com>
2025-07-25 13:03:59 +0530

    Update otpRoutes.js

--- routes/otpRoutes.js
+++ routes/otpRoutes.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import {
    sendOTP,
-   verifyOTP
+   verifyOTP,
+    resendOTP,
 }
    from '../controllers/otpController.js';
 
 const otpRoutes = express.Router();
 otpRoutes.post('/userphoneverifiy', sendOTP);
 otpRoutes.post('/otpvarification', verifyOTP);
+otpRoutes.post('/resendotp', resendOTP);
 
 export default otpRoutes;
