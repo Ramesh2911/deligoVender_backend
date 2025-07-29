56a9023f35323010723e70f38771e2a480e7e930
Ramesh Jana <81910860+Ramesh2911@users.noreply.github.com>
2025-07-29 19:45:27 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,12 +2,20 @@ import express from 'express';
 import {
    login,
    logout,
-   getCountries
+   getCountries,
+   createAcount,
+   updateUserAccount
 }
    from '../controllers/authController.js';
 import { adminAuth } from '../middleware/auth.js';
+import multer from 'multer';
 
 const authRoutes = express.Router();
+
+const upload = multer();
+
+authRoutes.post('/createuseraccount', upload.none(), createAcount);
+authRoutes.put('/useraccountupdate', upload.none(), updateUserAccount);
 authRoutes.post('/login', login);
 authRoutes.post('/logout', logout);
 authRoutes.get('/country-list', getCountries);
