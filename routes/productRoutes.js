98541fc64d5b5c603fc1fc702b78d4f0934e4706
Ramesh Jana <81910860+Ramesh2911@users.noreply.github.com>
2025-07-30 16:39:00 +0530

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,12 +3,14 @@ import { adminAuth } from '../middleware/auth.js';
 
 import {
    addProduct,
+   updateProduct,
    getProductsByVendor
 }
    from '../controllers/productController.js';
 
 const productRoutes = express.Router();
 productRoutes.post('/addProduct', addProduct);
+productRoutes.put('/updateProduct/:pid', updateProduct);
 productRoutes.get('/products-by-vendor', getProductsByVendor);
 
 export default productRoutes;
