d374176a0b605d4e25d1b5e786c7ff5d6cda97d4
Ramesh Jana <81910860+Ramesh2911@users.noreply.github.com>
2025-08-12 15:27:42 +0530

    Update dashboardRoutes.js

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import {
    vendorStats,
-   updateShopStatus
+   updateShopStatus,
+   getOrders,
+   updateNotification
 }
    from '../controllers/dashboardController.js';
 import { verifyToken } from '../middleware/auth.js';
@@ -9,5 +11,7 @@ import { verifyToken } from '../middleware/auth.js';
 const dashboardRoutes = express.Router();
 dashboardRoutes.get('/vendor-stats', verifyToken, vendorStats);
 dashboardRoutes.put('/shop-status', verifyToken, updateShopStatus);
+dashboardRoutes.get('/get-orders', verifyToken, getOrders);
+dashboardRoutes.put('/update-notification', verifyToken, updateNotification);
 
 export default dashboardRoutes;
