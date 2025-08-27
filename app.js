ee4f32976975ea70887de8e7d11c95099c589788
Ramesh Jana <81910860+Ramesh2911@users.noreply.github.com>
2025-08-27 13:46:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ import unitRoutes from './routes/unitRoutes.js';
 import dashboardRoutes from './routes/dashboardRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
 
+import Stripe from "stripe";
+
 config({
     path: './config.env'
 });
@@ -32,5 +34,33 @@ app.use('/api', productRoutes);
 app.use('/api', unitRoutes);
 app.use('/api', dashboardRoutes);
 app.use('/api', orderRoutes);
+
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+app.post('/payment-sheet', async (req, res) => {
+    const customer = await stripe.customers.create();
+    const ephemeralKey = await stripe.ephemeralKeys.create(
+        { customer: customer.id },
+        { apiVersion: '2025-07-30.basil' }
+    );
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: 1099,
+        currency: 'eur',
+        customer: customer.id,
+        automatic_payment_methods: {
+            enabled: true,
+        },
+    });
+
+    res.json({
+        paymentIntent: paymentIntent.client_secret,
+        ephemeralKey: ephemeralKey.secret,
+        customer: customer.id,
+        publishableKey: 'pk_test_51RyWnhAcEylHxGbjiFCv3x0mDXcxixAm6L6X7BvTi1tnD9iQs4ko7N8iAGSVZlbi4pq74NBQkO0q4t1hSYNaeLf000LjYLOVtK'
+    });
+});
+
+
 
 export default app;
